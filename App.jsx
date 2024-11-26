b512a316d8efd74c204ea076c5deb5a7c1831ce8
Jhon E. Acosta <120440528+Kerinc1@users.noreply.github.com>
2024-11-26 05:55:03 -0500

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -2,9 +2,16 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Login'; // Asegúrate de que el componente Login exista
 import ProductList from './inventory'; // Asegúrate de que el componente Inventory exista
-import Register from './register';
+import Register from './register'; // Asegúrate de que el componente Register exista
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  // Función para manejar el login
+  const handleLogin = (user) => {
+    setUser(user); // Guardamos el usuario en el estado
+  };
+
   return (
     <Router>
       <div className="App">
@@ -12,8 +19,12 @@ function App() {
         <Routes>
           {/* Ruta principal */}
           <Route path="/" element={<Home />} />
+
           {/* Ruta para el Login */}
-          <Route path="/login" element={<Login />} />
+          <Route
+            path="/login"
+            element={<Login onLogin={handleLogin} />} // Pasamos handleLogin a Login
+          />
 
           <Route path="/register" element={<Register />} />
 
@@ -33,9 +44,8 @@ const Home = () => {
       <a href="/login">Ir a Login</a>
       <br />
       <a href="/inventory">Ir a Inventario</a>
-      <br/>
+      <br />
       <a href="/register">Ir a registro</a>
-
     </div>
   );
 };
